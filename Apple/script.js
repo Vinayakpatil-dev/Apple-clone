72870a7ec048351f5cb314d431f37cf5467a9391
Vinayak Patil <69285967+Vinayakpatil-dev@users.noreply.github.com>
2025-02-17 16:28:09 +0530

    Update script.js
    Sections also redirect like the original website.

--- Apple/script.js
+++ Apple/script.js
@@ -1,18 +1,29 @@
 document.getElementById("b1").onclick = function(){
-    location.href="https://www.apple.com/in/shop/buy-iphone/iphone-16-pro"
+    location.href="https://www.apple.com/in/shop/buy-iphone/iphone-16-pro";
 }
 document.getElementById("b2").onclick = function(){
-    location.href="https://www.apple.com/in/shop/buy-iphone/iphone-16"
+    location.href="https://www.apple.com/in/shop/buy-iphone/iphone-16";
 }
 document.getElementById("b3").onclick = function(){
-    location.href="https://www.apple.com/in/shop/buy-watch/apple-watch"
+    location.href="https://www.apple.com/in/shop/buy-watch/apple-watch";
 }
+
 document.getElementById("a1").onclick = function(){
-    location.href="https://www.apple.com/in/iphone-16-pro/"
+    location.href="https://www.apple.com/in/iphone-16-pro/";
 }
 document.getElementById("a2").onclick = function(){
-    location.href="https://www.apple.com/in/iphone-16/"
+    location.href="https://www.apple.com/in/iphone-16/";
 }
 document.getElementById("a3").onclick = function(){
-    location.href="https://www.apple.com/in/apple-watch-series-10/"
+    location.href="https://www.apple.com/in/apple-watch-series-10/";
+}
+
+document.getElementById("s1").onclick = function(){
+    location.href="https://www.apple.com/in/iphone-16-pro/";
+}
+document.getElementById("s2").onclick = function(){
+    location.href="https://www.apple.com/in/iphone-16/";
+}
+document.getElementById("s3").onclick = function(){
+    location.href="https://www.apple.com/in/apple-watch-series-10/";
 }
